d8d8b695bdddbcacc81d2c541fc6dc780feafc94
Bekkkkkkk <franciscobuj@hotmail.com>
2023-10-22 22:33:52 -0300

    slide music y sfx

---
+++ Assets/Scripts/MenuScripts/Volume.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class Volume : MonoBehaviour
+{
+    [SerializeField] AudioMixer mixer;
+    [SerializeField] Slider musicSlider;
+    [SerializeField] Slider sfxSlider;
+
+    const string MIXER_MUSIC = "MusicVolume";
+    const string MIXER_SFX = "SfxVolume";
+
+    void Awake()
+    {
+        musicSlider.onValueChanged.AddListener(SetMusicVolume);
+        sfxSlider.onValueChanged.AddListener(SetSFXVolume);
+    }
+
+    void SetMusicVolume(float value)
+    {
+        mixer.SetFloat(MIXER_MUSIC, Mathf.Log10(value) * 20);
+
+    }
+
+    void SetSFXVolume(float value)
+    {
+        mixer.SetFloat(MIXER_SFX, Mathf.Log10(value) * 20);
+    }
+}
