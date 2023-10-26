f8b0b39acd2b5c8f3f7231636d54a88286340efd
Bekkkkkkk <franciscobuj@hotmail.com>
2023-10-26 18:56:42 -0300

    cinematica + run

---
+++ Assets/Scripts/EnemyScripts/TriggerCollider.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerCollider : MonoBehaviour
+{
+    public GameObject enemy;  // El GameObject del enemigo
+    public float activationDelay = 2f;  // Retraso en segundos antes de activar el enemigo
+    public AudioSource audioSource;  // El AudioSource para la música
+    public Animator cinematicAnimator;  // El Animator para la animación "cinemática"
+    public GameObject meshObject;  // El GameObject que contiene el Mesh Renderer
+    public GameObject player;
+
+    private bool triggered = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !triggered)
+        {
+            // Activa el MeshRenderer del hijo
+            meshObject.SetActive(true);
+            player.SetActive(false);
+
+            // Activa el AudioSource para la música
+            audioSource.Play();
+
+            // Activa la animación "cinemática"
+            cinematicAnimator.SetTrigger("PlayCinematic");
+
+            // Inicia una corrutina para activar el enemigo después del retraso
+            StartCoroutine(ActivateEnemyAfterDelay());
+
+            triggered = true;
+        }
+    }
+
+    private IEnumerator ActivateEnemyAfterDelay()
+    {
+        yield return new WaitForSeconds(activationDelay);
+        player.SetActive(true);
+        // Llama a la función ActivateEnemy en el script del enemigo
+        enemy.GetComponent<ChaserF>().ActivateEnemy();
+    }
+}
