08af77ed9a0bece3a82fec2a2fa2fbe0ce1446fe
NachoIannizzotto <60115738+NachoIannizzotto@users.noreply.github.com>
2023-10-26 17:58:54 -0300

    Final Chaser

---
+++ Assets/Assets/Enemy/ForwardChaser/ChaserF.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChaserF : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public bool isActive = false;
+    public string deathScene;
+    public Camera jumpscareCamera;
+    public Animator aiAnim;
+    public float jumpscareTime;
+
+    private void FixedUpdate()
+    {
+        if (isActive)
+        {
+            aiAnim.SetTrigger("sprint");
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+    }
+
+    public void ActivateEnemy()
+    {
+        isActive = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (isActive && other.CompareTag("Player"))
+        {
+            HandlePlayerCollision();
+        }
+    }
+
+    private void HandlePlayerCollision()
+    {
+        aiAnim.SetTrigger("jumpscare");
+        StartCoroutine(deathRoutine());
+    }
+
+    IEnumerator deathRoutine()
+    {
+        jumpscareCamera.gameObject.SetActive(true);
+        yield return new WaitForSeconds(jumpscareTime);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        SceneManager.LoadScene(deathScene);
+    }
+}
+
+
+
